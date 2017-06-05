b2d73dae57b7bdd5a955823e05c8f39b5d6aad0a
Yikai <ywang8@stuy.edu>
2017-06-05 15:29:40 -0400

    added some math fxns

--- gmath.py
+++ gmath.py
@@ -5,7 +5,7 @@ def calculate_normal(polygons, i):
     A = [0, 0, 0]
     B = [0, 0, 0]
     N = [0, 0, 0]
-    
+
     A[0] = polygons[i+1][0] - polygons[i][0]
     A[1] = polygons[i+1][1] - polygons[i][1]
     A[2] = polygons[i+1][2] - polygons[i][2]
@@ -19,3 +19,20 @@ def calculate_normal(polygons, i):
     N[2] = A[0] * B[1] - A[1] * B[0]
 
     return N
+
+def calculate_mag(vector):
+    return math.sqrt( vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2] )
+
+def to_unit_vector(vector):
+    v = [0, 0, 0]
+    mag = calculate_mag(vector)
+    v[0] = (vector[0])/mag
+    v[1] = (vector[1])/mag
+    v[2] = (vector[2])/mag
+    return v
+
+def calculate_dot_product(a, b):
+    a = to_unit_vector(a)
+    b = to_unit_vector(b)
+
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
